1f9373845d658ca59cb53a7e59c8566cf82a9cff
chiba <edyzrg1118@gmail.com>
2025-02-25 04:08:49 +0900

    Userモデル、ルーティング、スキーマの変更,

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
+  # 1人の User は複数の Board を持つことができる。 User が削除されたら、その User に紐づく Board も一緒に削除される。
+  has_many :boards, dependent: :destroy
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
